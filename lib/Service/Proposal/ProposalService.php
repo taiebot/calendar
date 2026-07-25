c5e166c8253e0d552d32757410e97201cd733404
taiebot <13300652+taiebot@users.noreply.github.com>
2026-07-25 09:02:32 +0200

    Refactor proposal response notification structure
    Removed 'type' from the proposal response and added 'participantId' and 'participantName'.
    
    Signed-off-by: taiebot <13300652+taiebot@users.noreply.github.com>

--- lib/Service/Proposal/ProposalService.php
+++ lib/Service/Proposal/ProposalService.php
@@ -461,12 +461,9 @@ class ProposalService {
 			->setObject('proposal', (string)$proposalEntry->getId())
 			->setSubject('proposal_response', [
 				'id' => $proposalEntry->getId(),
-				'type' => 'higlight',
 				'name' => $proposalEntry->getTitle(),
-			])
-			->setMessage('proposal_response', [
-				'id' => $participantEntry->getId(),
-				'participant' => $participantEntry->getName() ?? $participantEntry->getAddress(),
+				'participantId' => $participantEntry->getId(),
+				'participantName' => $participantEntry->getName() ?? $participantEntry->getAddress(),
 			]);
 		$this->notificationManager->notify($notification);
 	}
